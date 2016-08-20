4a3e1b88802bb0572dfdf0bb388e1fd35ae1c86e
lulalachen <lulalachen@gmail.com>
2016-08-20 16:48:29 +0800

    feat(rightPanel): ui

--- app/components/components.js
+++ app/components/components.js
@@ -1,6 +1,7 @@
+import rightPanelComponent from './rightPanel/rightPanel';
 import homeComponent from './home/home';
 import heroComponent from './hero/hero';
 
 export default app => {
-  INCLUDE_ALL_MODULES([homeComponent, heroComponent], app);
+  INCLUDE_ALL_MODULES([homeComponent, heroComponent, rightPanelComponent], app);
 }
