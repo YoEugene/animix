8e1e1421a3632473eae6baa3aea4e7e8079970e3
YoEugene <dreammacer.yo@gmail.com>
2016-08-20 20:47:52 +0800

    add Buy from Store to Zoo function

--- app/services/services.js
+++ app/services/services.js
@@ -1,5 +1,6 @@
+import animalsService from './animals/animals';
 import usersService from './users/users';
 
 export default app => {
-  INCLUDE_ALL_MODULES([usersService], app);
+  INCLUDE_ALL_MODULES([usersService, animalsService], app);
 }
